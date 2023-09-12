1773bc218cb303c5f1896b4a79f14456ae21f6cc
KotaYamazaki0203 <k_yamazaki@craid-inc.com>
2023-09-12 14:51:03 +0900

    Entity should not have the dependence of gorm

--- app/src/entities/todo.go
+++ app/src/entities/todo.go
@@ -1,15 +1,14 @@
 package entities
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
 type Todo struct {
-	ID        uint           `json:"id"`
-	Title     string         `json:"title"`
-	Content   string         `json:"content"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	ID        uint      `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
