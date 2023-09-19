21e86393dcc48d69b35a41d0bab0faa33dcb9f6a
KotaYamazaki0203 <k_yamazaki@craid-inc.com>
2023-09-19 09:50:14 +0900

    Use CamelCase to define struct fields

--- app/src/model/todos.go
+++ app/src/model/todos.go
@@ -7,8 +7,8 @@ import (
 
 type Todos struct {
 	ID        uint
-	TITLE     string    `gorm:"column:title;type:varchar(255);not null"`
-	CONTENT   string    `gorm:"column:content;not null"`
+	Title     string    `gorm:"column:title;type:varchar(255);not null"`
+	Content   string    `gorm:"column:content;not null"`
 	CreatedAt time.Time `gorm:"not null"`
 	UpdatedAt time.Time `gorm:"not null"`
 	DeletedAt gorm.DeletedAt
